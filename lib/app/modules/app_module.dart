1a3b93cea82b6a72ddfe0b0bf2cbf1ce3f933a14
Adby Santos <adby.santos15@outlook.com>
2022-05-11 10:16:45 -0300

    removing not used imports

--- lib/app/modules/app_module.dart
+++ lib/app/modules/app_module.dart
@@ -1,8 +1,5 @@
-import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-import '../core/constants/endpoints.dart';
-import '../core/services/http_client/dio/dio_http_client.dart';
 import 'home/home_module.dart';
 
 class AppModule extends Module {
